39b557edfce30d55c895fd286eab1b6154c29f35
Abhinandan Mishra <abhinandanmishra360@gmail.com>
2022-06-01 11:33:42 +0530

    added second version

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-window.addEventListener('onkeydown',function(e){
-    // const audio=document.
-    if(e.key!=undefined){
-        const audio=document.querySelector(`audio[data-key="${e.keycode}"]`);
-        console.log(audio);
-        console.log(e.keyCode);
-    }
-})
+
